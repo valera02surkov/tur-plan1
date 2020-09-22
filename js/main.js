a2e773ec51d1de63c718879a5e1a9b73327fa7fb
valera02surkov <AKIGR1@list.ru>
2020-09-23 02:27:18 +0500

    creted: java maps

--- js/main.js
+++ js/main.js
@@ -12,3 +12,22 @@ var mySwiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 });
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init() {
+  myMap = new ymaps.Map(
+    "map",
+    {
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: (7.838184812206101, 98.29880496908747),
+      zoom: 10,
+    },
+    {
+      searchControlProvider: "yandex#search",
+    }
+  );
+}
