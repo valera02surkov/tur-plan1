7e40f7b57ec0dcca969320ea35e5b6ff9f9be9c3
valera02surkov <AKIGR1@list.ru>
2020-10-02 17:54:55 +0500

    creted: madal

---
+++ send.php
@@ -0,0 +1,55 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['phone'];
+$message = $_POST['message'];
+// Формирование самого письма
+$title = "Новое обращение Best-tour-plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'valer74dhon@gmail.com '; // Логин на почте
+    $mail->Password   = 'sdi19820702sdi'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('valer74dhon@gmail.com', 'Валера 74'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('AKIGR1@list.ru');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результат
+header('location: thenkyoo.html');
